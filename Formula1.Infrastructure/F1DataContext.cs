37f230324764222342b0275c7b30d92188ab2963
Sunny Patel <73701502+sunnypatel2048@users.noreply.github.com>
2023-07-06 22:39:44 +0530

    Created EF Models and Migrations (#2)

---
+++ Formula1.Infrastructure/F1DataContext.cs
@@ -0,0 +1,59 @@
+﻿using Formula1.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formula1.Infrastructure
+{
+    /// <summary>The F1DataContextClass</summary>
+    public class F1DataContext : DbContext
+    {
+        /// <summary>Initializes a new instance of the <see cref="F1DataContext" /> class.</summary>
+        /// <param name="options">The options.</param>
+        public F1DataContext(DbContextOptions<F1DataContext> options) : base(options) { }
+
+        /// <summary>
+        /// Override this method to further configure the model that was discovered by convention from the entity types
+        /// exposed in <see cref="T:Microsoft.EntityFrameworkCore.DbSet`1">DbSet</see> properties on your derived context. The resulting model may be cached
+        /// and re-used for subsequent instances of your derived context.
+        /// </summary>
+        /// <param name="modelBuilder">
+        /// The builder being used to construct the model for this context. Databases (and other extensions) typically
+        /// define extension methods on this object that allow you to configure aspects of the model that are specific
+        /// to a given database.
+        /// </param>
+        /// <remarks>
+        ///   <para>
+        /// If a model is explicitly set on the options for this context (via <see cref="M:Microsoft.EntityFrameworkCore.DbContextOptionsBuilder.UseModel(Microsoft.EntityFrameworkCore.Metadata.IModel)">UseModel(Microsoft.EntityFrameworkCore.Metadata.IModel)</see>)
+        /// then this method will not be run. However, it will still run when creating a compiled model.
+        /// </para>
+        ///   <para>
+        /// See <a href="https://aka.ms/efcore-docs-modeling">Modeling entity types and relationships</a> for more information and
+        /// examples.
+        /// </para>
+        /// </remarks>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseSerialColumns();
+        }
+
+        /// <summary>Gets or sets the drivers.</summary>
+        /// <value>The drivers.</value>
+        public DbSet<Driver> Drivers { get; set; }
+
+        /// <summary>Gets or sets the teams.</summary>
+        /// <value>The teams.</value>
+        public DbSet<Team> Teams { get; set; }
+
+        /// <summary>Gets or sets the engine providers.</summary>
+        /// <value>The engine providers.</value>
+        public DbSet<EngineProvider> EngineProviders { get; set; }
+
+        /// <summary>Gets or sets the team principals.</summary>
+        /// <value>The team principals.</value>
+        public DbSet<TeamPrincipal> TeamPrincipals { get; set; }
+    }
+}
---
+++ Formula1.Infrastructure/Models/Team.cs
@@ -0,0 +1,70 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Formula1.Infrastructure.Models
+{
+    /// <summary>The Team Class</summary>
+    public class Team
+    {
+        /// <summary>Gets or sets the identifier.</summary>
+        /// <value>The identifier.</value>
+        public int Id { get; set; }
+
+        /// <summary>Gets or sets the name.</summary>
+        /// <value>The name.</value>
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>Gets or sets the full name.</summary>
+        /// <value>The full name.</value>
+        [Required]
+        public string FullName { get; set; }
+
+        /// <summary>Gets or sets the base.</summary>
+        /// <value>The base.</value>
+        public string Base { get; set; }
+
+        /// <summary>Gets or sets the race entered.</summary>
+        /// <value>The race entered.</value>
+        public int RaceEntered { get; set; }
+
+        /// <summary>Gets or sets the race wins.</summary>
+        /// <value>The race wins.</value>
+        public int RaceWins { get; set; }
+
+        /// <summary>Gets or sets the podiums.</summary>
+        /// <value>The podiums.</value>
+        public int Podiums { get; set; }
+
+        /// <summary>Gets or sets the pole positions.</summary>
+        /// <value>The pole positions.</value>
+        public int PolePositions { get; set; }
+
+        /// <summary>Gets or sets the constructor championships.</summary>
+        /// <value>The constructor championships.</value>
+        public int ConstructorChampionships { get; set; }
+
+        /// <summary>Gets or sets the website.</summary>
+        /// <value>The website.</value>
+        public string? Website { get; set; }
+
+        /// <summary>Gets or sets the current chassis.</summary>
+        /// <value>The current chassis.</value>
+        public string? CurrentChassis { get; set; }
+
+        /// <summary>Gets or sets the current engine provider.</summary>
+        /// <value>The current engine provider.</value>
+        public EngineProvider? CurrentEngineProvider { get; set; }
+
+        /// <summary>Gets or sets the current team principal.</summary>
+        /// <value>The current team principal.</value>
+        public TeamPrincipal? CurrentTeamPrincipal { get; set; }
+
+        /// <summary>Gets or sets the created time.</summary>
+        /// <value>The created time.</value>
+        public DateTime CreatedTime { get; set; }
+
+        /// <summary>Gets or sets the modified time.</summary>
+        /// <value>The modified time.</value>
+        public DateTime? ModifiedTime { get; set; }
+    }
+}
