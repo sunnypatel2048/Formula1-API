836e97d45e901e3ea7542b02c5d69c6f6ea4970c
Sunny Patel <73701502+sunnypatel2048@users.noreply.github.com>
2023-07-19 02:23:38 +0530

    Created GET endpoints (#4)
    * Created GET endpoints for Drivers
    
    * Created GET endpoints for EngineProviders
    
    * Created GET endpoints for TeamPrincipals
    
    * Created GET endpoints for Teams

---
+++ Formula1/Controllers/TeamPrincipalsController.cs
@@ -0,0 +1,48 @@
+﻿using Formula1.Infrastructure;
+using Formula1.Infrastructure.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Formula1.Controllers
+{
+    /// <summary>The TeamPrincipalsController Class</summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeamPrincipalsController : ControllerBase
+    {
+        private readonly F1DataContext _context;
+
+        /// <summary>Initializes a new instance of the <see cref="TeamPrincipalsController" /> class.</summary>
+        /// <param name="dataContext">The data context.</param>
+        public TeamPrincipalsController(F1DataContext dataContext)
+        {
+            _context = dataContext;
+        }
+
+        /// <summary>Gets all the team principals.</summary>
+        /// <returns>List of all team principals</returns>
+        [HttpGet]
+        public async Task<ActionResult<List<TeamPrincipal>>> Get()
+        {
+            var teamPrincipals = await _context.TeamPrincipals.ToListAsync();
+            return Ok(teamPrincipals);
+        }
+
+        /// <summary>Gets the team principal by specified identifier.</summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns>Team Principal by specified identifier</returns>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TeamPrincipal>> Get(int id)
+        {
+            var teamPrincipal = await _context.TeamPrincipals.FindAsync(id);
+
+            if (teamPrincipal == null)
+            {
+                return BadRequest("Team Principal not found.");
+            }
+
+            return Ok(teamPrincipal);
+        }
+    }
+}
---
+++ Formula1/Controllers/TeamsController.cs
@@ -0,0 +1,52 @@
+﻿using Formula1.Infrastructure;
+using Formula1.Infrastructure.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Formula1.Controllers
+{
+    /// <summary>The TeamsController Class</summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeamsController : ControllerBase
+    {
+        private readonly F1DataContext _context;
+
+        /// <summary>Initializes a new instance of the <see cref="TeamsController" /> class.</summary>
+        /// <param name="dataContext">The data context.</param>
+        public TeamsController(F1DataContext dataContext)
+        {
+            _context = dataContext;
+        }
+
+        /// <summary>Gets all the teams.</summary>
+        /// <returns>List of all teams</returns>
+        [HttpGet]
+        public async Task<ActionResult<List<Team>>> Get()
+        {
+            var teams = await _context.Teams.Include(t => t.CurrentTeamPrincipal)
+                                            .Include(t => t.CurrentEngineProvider)
+                                            .ToListAsync();
+            return Ok(teams);
+        }
+
+        /// <summary>Gets the team by specified identifier.</summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns>Team by specified identifier</returns>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Team>> Get(int id)
+        {
+            var team = await _context.Teams.Include(t => t.CurrentTeamPrincipal)
+                                            .Include(t => t.CurrentEngineProvider)
+                                            .FirstOrDefaultAsync(t => t.Id == id);
+
+            if (team == null)
+            {
+                return BadRequest("Team not found.");
+            }
+
+            return Ok(team);
+        }
+    }
+}
